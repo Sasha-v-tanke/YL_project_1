b3b9e4f6ea20b8cc6bfea9e88de2cb8b2a6e247a
Александр <s4sha-v-tanke@yandex.ru>
2022-10-21 21:52:09 +0300

    smthng

--- YL_project_1/YL_project_1.py
+++ YL_project_1/YL_project_1.py
@@ -1,1 +1,36 @@
+import sys
 
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import QtCore, QtGui
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('WelcomeWindow.ui', self)
+        self.AdminAccount.clicked.connect(self.showAdminMenu)
+        self.NewOrder.clicked.connect(self.openUserWindow)
+
+    def showAdminMenu(self):
+        uic.loadUi('AdminWelcome.ui', self)
+        self.AdminAccount.setText("OK")
+        self.CheckPassword.clicked.connect(self.checkAdminAccount)
+
+    def checkAdminAccount(self):
+        login = self.Login.text()
+        password = self.Password.text()
+        self.CheckPassword.setText(login + password)
+
+    def openAdminWindow(self):
+        pass
+
+    def openUserWindow(self):
+        self.NewOrder.setText("ok")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
